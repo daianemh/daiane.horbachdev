d46f3d9420e6c7adb13df369730cae0fd52ec2d1
Daiane Moreira Horbach <daianemoreirahorbach@gmail.com>
2025-06-08 19:52:59 -0400

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const canvas = document.getElementById("fundo");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// Ajusta o tamanho do canvas para o tamanho da janela e escala para retina
+function resizeCanvas() {
+  const dpr = window.devicePixelRatio || 1;
+  canvas.width = window.innerWidth * dpr;
+  canvas.height = window.innerHeight * dpr;
+  canvas.style.width = window.innerWidth + "px";
+  canvas.style.height = window.innerHeight + "px";
+  ctx.scale(dpr, dpr);
+}
+
+resizeCanvas();
 
 let particlesArray = [];
 
 class Particle {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
+    this.x = Math.random() * window.innerWidth;
+    this.y = Math.random() * window.innerHeight;
     this.size = Math.random() * 2 + 1;
     this.speedX = Math.random() * 1 - 0.5;
     this.speedY = Math.random() * 1 - 0.5;
@@ -19,8 +28,9 @@ class Particle {
     this.x += this.speedX;
     this.y += this.speedY;
 
-    if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
-    if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+    // Inverte a direção ao atingir as bordas da janela
+    if (this.x < 0 || this.x > window.innerWidth) this.speedX *= -1;
+    if (this.y < 0 || this.y > window.innerHeight) this.speedY *= -1;
   }
 
   draw() {
@@ -34,15 +44,16 @@ class Particle {
 function init() {
   particlesArray = [];
   const numParticles = 150;
-
   for (let i = 0; i < numParticles; i++) {
     particlesArray.push(new Particle());
   }
 }
 
 function animate() {
+  // Limpa o canvas inteiro
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Atualiza e desenha cada partícula
   for (let particle of particlesArray) {
     particle.update();
     particle.draw();
@@ -51,11 +62,13 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
+// Reajusta o canvas e reinicia as partículas ao redimensionar a janela
 window.addEventListener("resize", () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  resizeCanvas();
   init();
 });
 
+// Inicializa e começa a animação
 init();
 animate();
+
